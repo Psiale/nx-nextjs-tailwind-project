1f6b17bf2cb3f5ed8b9efaf1b63242344da344ec
psiale <psialedev@gmail.com>
2022-04-27 11:43:34 -0500

    working on nextjs

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+
+import { supabase } from '../utils/supabase';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      Working
+    </div>
+  )
+}
+
+export async function getStaticPros(context) {
+  return {
+
+  }
+}
+
+export default Home
